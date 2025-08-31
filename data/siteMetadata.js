4f879767a466cf855faf3c7a8ad222dc2c8aa7e7
5tatusQuo <isuquo@gmail.com>
2025-08-31 17:22:51 +1000

    Remove newsletter API route and configuration completely

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -39,11 +39,7 @@ const siteMetadata = {
     //   googleAnalyticsId: '', // e.g. G-XXXXXXX
     // },
   },
-  newsletter: {
-    // supports mailchimp, buttondown, convertkit, klaviyo, revue, emailoctopus, beehive
-    // Please add your .env file and modify it according to your selection
-    provider: '', // Set to empty string to disable newsletter functionality
-  },
+
   //comments: {
     // If you want to use an analytics provider you have to add it to the
     // content security policy in the `next.config.js` file.
